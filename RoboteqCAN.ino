31daf14584f44d4f9e3a3a99d91e7e269a465f0b
Kamran14 <Kamran.tayyab99@gmail.com>
2020-01-30 17:42:22 -0500

    Updated Read/Write Objects

---
+++ RoboteqCAN.ino
@@ -0,0 +1,63 @@
+//!G 1 500 
+//{Command, Motor ID, int32(-1000<N<1000)} ;-> Refer to pg.18
+//TODO:
+
+#include <SPI.h>
+#include <mcp2515.h>
+
+struct can_frame writingObject;
+struct can_frame readObject;
+MCP2515 mcp2515(10);
+
+
+void setup() {
+
+  writingObject.can_id  = 0x0F6;
+  writingObject.can_dlc = 8;
+  writingObject.data[0] = 0x8E;
+  writingObject.data[1] = 0x87;
+  writingObject.data[2] = 0x32;
+  writingObject.data[3] = 0xFA;
+  writingObject.data[4] = 0x26;
+  writingObject.data[5] = 0x8E;
+  writingObject.data[6] = 0xBE;
+  writingObject.data[7] = 0x86;
+
+ 
+  
+  while (!Serial);
+  Serial.begin(115200);
+  SPI.begin();
+  
+  mcp2515.reset();
+  mcp2515.setBitrate(CAN_125KBPS);
+  mcp2515.setNormalMode();
+  
+  Serial.println("Example: Write to CAN");
+  mcp2515.sendMessage(&writingObject);
+}
+
+void loop() {
+  
+  
+  
+
+  
+   if (mcp2515.readMessage(&readObject) == MCP2515::ERROR_OK) {
+      
+    Serial.print(readObject.can_id, HEX); // print ID
+    Serial.print(" "); 
+    Serial.print(readObject.can_dlc, HEX); // print DLC
+    Serial.print(" ");
+    
+    for (int i = 0; i<readObject.can_dlc; i++)  {  // print the data
+        
+      Serial.print(readObject.data[i],HEX);
+      Serial.print(" ");
+
+    }
+
+    Serial.println();      
+  }
+
+}
